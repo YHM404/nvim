3731e380dd3843ca05a620485a59cf835738e986
yaohuiming <yaohuiming789@gmail.com>
2023-03-05 01:44:23 +0800

    update hop keymap

--- lua/keymap/hop.lua
+++ lua/keymap/hop.lua
@@ -2,8 +2,12 @@ local hop = require('hop')
 local directions = require('hop.hint').HintDirection
 
 vim.keymap.set('', 'f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
 end, {remap=true})
 vim.keymap.set('', 'F', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
 end, {remap=true})
+vim.keymap.set('', '<leader><leader>', function()
+  hop.hint_words({ keys = 'asdfhjkln' })
+end, {remap=true})
+
